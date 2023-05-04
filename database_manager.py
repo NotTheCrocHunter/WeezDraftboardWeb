e1c123d1a07b19dd807e9f504529c81697a0973b
NotTheCrocHunter <notthecrochunter@gmail.com>
2023-05-04 08:30:54 -0400

    Remove the Player class from the sleeper_wrapper init.  Add docker connect string to database_manager.

--- database_manager.py
+++ database_manager.py
@@ -1,23 +1,36 @@
 from sleeper_wrapper import Players
 import psycopg2
- 
-# connection establishment
+
+
+""" 
+# connection establishment version 1
 conn = psycopg2.connect(
    database="postgres",
     user='postgres',
     password='postgrespw',
     host='localhost',
-    port= '32768'
+    port='32768'
 )
- 
+ """
+
+conn = psycopg2.connect(
+    database='postgres',
+    user='postgres',
+    password='docker',
+    port='5432',
+    host='localhost')
+
 conn.autocommit = True
-postgres_string = 'postgres://postgres:postgrespw@localhost:32768' 
+postgres_string = 'postgres://postgres:postgrespw@localhost:32768'
+docker_string = 'docker run --name fantasy-postgres-db -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres'
 # Creating a cursor object
 cursor = conn.cursor()
  
 # query to create a database
-sql = ''' CREATE database products ''';
- 
+sql = ''' CREATE database players ''';
+# need sql query to create the table if it doesn't exist and add the players to it
+
+
 # executing above query
 cursor.execute(sql)
 print("Database has been created successfully !!");
